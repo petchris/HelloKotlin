47be2d9a832154066c06f1a3484270b39e62798d
petchris <vilhelm.lacour@gmail.com>
2019-11-18 11:48:18 +0100

    Changes from part 4-6

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val strings = arrayOf("one", "four")
+        println(strings[1])
+
+        doSomething()
+
+        Log.d( TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d( TAG , "onStart")
+    }
+
+    fun doSomething() {
+        println("Hello")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d( TAG , "onResume")
     }
 }
